e3a5164ef2307f9f81801ea0ec9fbb786690b313
nygeog <daniel.martin.sheehan@gmail.com>
2014-08-04 09:06:00 -0400

    update

--- 01-taxi-add-date-clean.py
+++ 01-taxi-add-date-clean.py
@@ -2,6 +2,8 @@ import pandas as pd
 import datetime
 import os
 
+wd = '/Users/danielmsheehan/Desktop/tripData2013/' #work dir, create folder called day in your work dir
+
 def date_range(start, end):
     r = (end+datetime.timedelta(days=1)-start).days
     return [start+datetime.timedelta(days=i) for i in range(r)]
@@ -9,44 +11,50 @@ def date_range(start, end):
 dropList = ['medallion','hack_license','vendor_id','rate_code','store_and_fwd_flag','passenger_count','trip_time_in_secs']
 typeList = ['pickup','dropoff']
 
-# for i in range(2,13):
-# 	inCSV = '/Users/danielmsheehan/Desktop/tripData2013/trip_data_'+str(i)+'.csv'
-# 	df = pd.read_csv(inCSV)
-# 	df = df.rename(columns=lambda x: x.replace(' ', ''))
-# 	df = df.drop(dropList, axis=1) #nrows=200000
-# 	print 'read complete'
+print 'create a quick sample dataset'
+inCSVSamp = wd+'trip_data_1.csv'
+outCSVSamp = wd+'trip_data_1_samp.csv'
+dfS = pd.read_csv(inCSVSamp, nrows=100)
+dfS.to_csv(outCSVSamp, index=False)
 
-# 	print 'add new col'
-# 	for j in typeList:
-# 		df[j+'_datetime'] = df[j+'_datetime'].astype('datetime64[ns]')
-# 		df[j+'_date'] = df[j+'_datetime'].map(pd.Timestamp.date)
-# 		df = df.drop(j+'_datetime', axis=1)
+for i in range(2,13):
+	inCSV = wd+'trip_data_'+str(i)+'.csv'
+	df = pd.read_csv(inCSV)
+	df = df.rename(columns=lambda x: x.replace(' ', ''))
+	df = df.drop(dropList, axis=1) #nrows=200000
+	print 'read complete'
 
-# 	monthStart = i
-# 	yearStart  = 2013
-# 	if i > 11:
-# 		monthEnd = 1
-# 		yearEnd  = 2014
-# 	else:
-# 		monthEnd = i + 1
-# 		yearEnd  = 2013
+	print 'add new col'
+	for j in typeList:
+		df[j+'_datetime'] = df[j+'_datetime'].astype('datetime64[ns]')
+		df[j+'_date'] = df[j+'_datetime'].map(pd.Timestamp.date)
+		df = df.drop(j+'_datetime', axis=1)
 
-# 	start = datetime.date(yearStart,monthStart,01)
-# 	end = datetime.date(yearEnd,monthEnd,01)
-# 	dateList = date_range(start, end)
-# 	for k in dateList:
-# 		outCSV = '/Users/danielmsheehan/Desktop/tripData2013/day/trip_data_'+str(k).replace('-','')+'_m'+str(i)+'.csv'
-# 		#m = k.split('-')[0]
-# 		y = str(k)[0:4]
-# 		m = str(k)[5:7]
-# 		d = str(k)[8:10]
-# 		yearV = int(y)
-# 		monthV = int(m)
-# 		dayV = int(d)
-# 		dft = df[(df.pickup_date == datetime.date(yearV, monthV, dayV))]
-# 		print 'save as csv for ' + str(k)
-# 		#df = df.
-# 		dft.to_csv(outCSV, index=False)
+	monthStart = i
+	yearStart  = 2013
+	if i > 11:
+		monthEnd = 1
+		yearEnd  = 2014
+	else:
+		monthEnd = i + 1
+		yearEnd  = 2013
+
+	start = datetime.date(yearStart,monthStart,01)
+	end = datetime.date(yearEnd,monthEnd,01)
+	dateList = date_range(start, end)
+	for k in dateList:
+		outCSV = wd+'day/trip_data_'+str(k).replace('-','')+'_m'+str(i)+'.csv'
+		#m = k.split('-')[0]
+		y = str(k)[0:4]
+		m = str(k)[5:7]
+		d = str(k)[8:10]
+		yearV = int(y)
+		monthV = int(m)
+		dayV = int(d)
+		dft = df[(df.pickup_date == datetime.date(yearV, monthV, dayV))]
+		print 'save as csv for ' + str(k)
+		#df = df.
+		dft.to_csv(outCSV, index=False)
 	
 for i in range(9,13):
 	if i < 9:
@@ -57,6 +65,6 @@ for i in range(9,13):
 		k = '20140101'
 	else:
 		k = '2013'+ str(i+1) + '01'
-	delCSV = '/Users/danielmsheehan/Desktop/tripData2013/day/trip_data_'+k+'_m'+str(i)+'.csv'
+	delCSV = wd+'day/trip_data_'+k+'_m'+str(i)+'.csv'
 	os.remove(delCSV)
 
--- 02-csv-to-fgdb.py
+++ 02-csv-to-fgdb.py
@@ -15,8 +15,13 @@ for m in monthList:
                 tripDay = a_csv[38:46] #   Z:/Desktop/tripData2013/day/trip_data_20130201_m1.csv
                 print tripDay
                 gdb = wd+"taxi_2013"+m+".gdb"
+                print 'table to table - bring in csv'
                 arcpy.TableToTable_conversion(a_csv,gdb,"trip_"+tripDay)
+                print 'make xy event layer'
                 arcpy.MakeXYEventLayer_management(gdb+"/trip_"+tripDay,"pickup_longitude","pickup_latitude","trip_"+tripDay+"_Layer","GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433],METADATA['World',-180.0,-90.0,180.0,90.0,0.0,0.0174532925199433,0.0,1262]];-400 -400 1000000000;-100000 10000;-100000 10000;8.98315284119522E-09;0.001;0.001;IsHighPrecision","#")
+                print 'fc to fc'
                 arcpy.FeatureClassToFeatureClass_conversion("trip_"+tripDay+"_Layer",gdb,"trip_"+tripDay+"_p_xy")
+                print 'project to sp'
                 arcpy.Project_management(gdb+"/trip_"+tripDay+"_p_xy",gdb+"/trip_"+tripDay+"_p_xy_prj","PROJCS['NAD_1983_StatePlane_New_York_Long_Island_FIPS_3104_Feet',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Lambert_Conformal_Conic'],PARAMETER['False_Easting',984250.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-74.0],PARAMETER['Standard_Parallel_1',40.66666666666666],PARAMETER['Standard_Parallel_2',41.03333333333333],PARAMETER['Latitude_Of_Origin',40.16666666666666],UNIT['Foot_US',0.3048006096012192]]","WGS_1984_(ITRF00)_To_NAD_1983","GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433],METADATA['World',-180.0,-90.0,180.0,90.0,0.0,0.0174532925199433,0.0,1262]]")
+                print 'near analysis'
                 arcpy.Near_analysis(gdb+"/trip_"+tripDay+"_p_xy_prj","W:/GIS/Data/Municipal/USA/New_York/New_York_City/Streets_Centerline/20140609/CSCL_PUB_Centerline/CSCL_PUB_Centerline.shp","1000 Feet","LOCATION","ANGLE")
