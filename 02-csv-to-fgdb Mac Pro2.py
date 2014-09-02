f1c5ab51aef8cf0123e0c4daf02b7043cd2e0ffe
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2014-09-02 09:08:47 -0400

    update

--- 02-csv-to-fgdb Mac Pro2.py
+++ 02-csv-to-fgdb Mac Pro2.py
@@ -10,7 +10,7 @@ monthList = ['01','02','03','04','05','06','07','08','09','10','11','12']
 monthList = ['12']
 monthList = ['11']
 monthList = ['10']
-
+monthList = ['09']
 
 print 'for pickups and dropoffs'
 for m in monthList:
