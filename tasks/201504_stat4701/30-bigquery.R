fcf4af538736881ef165fcc62ac51effd6c23a9d
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2015-04-21 17:57:45 -0400

    update

--- tasks/201504_stat4701/30-bigquery.R
+++ tasks/201504_stat4701/30-bigquery.R
@@ -6,7 +6,7 @@ project <- "dazzling-will-91618" # put your project ID here
 
 # sql <- "SELECT COUNT(*) FROM [dazzling-will-91618:taxi_all.taxi_all_2013];" #This statement counts the number of rows. 
 sql_master <- "SELECT * FROM [dazzling-will-91618:taxi_all.taxi_all_2013] LIMIT 200" #THIS IS THE MASTER DATABASE
-sql_stats <- "SELECT * FROM [dazzling-will-91618:taxi_all.nycb2010_stats] LIMIT 200" #THIS IS THE GEOID CENSUS BLOCK 2010 dataset
+sql_stats <- "SELECT * FROM [dazzling-will-91618:taxi_all.nycb2010_stats]" #THIS IS THE GEOID CENSUS BLOCK 2010 dataset
 # COPY AND PASTE THE LINK THEY GIVE YOU TO YOUR BROWSER AND THEN ACCEPT WITH LOGIN TO GOOGLE AND PASTE THE AUTHORIZATION CODE THEY GIVE YOU
 
 dfm <- query_exec(sql_master, project = project)
